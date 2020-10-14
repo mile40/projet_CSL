b229e1d86b39a0957e39fd33a1b25de7d6b033ba
InakiUrrutia <urrutiainaki@wanadoo.fr>
2020-10-14 15:57:25 +0200

    Connection avec MongoDB

--- NantesPrisonManager/main.js
+++ NantesPrisonManager/main.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -7,6 +7,9 @@
 const express = require('express');
 const path = require('path');
 const port = process.env['PORT'] || 8080;
+
+const mongoClient = require('mongodb').MongoClient; // Je test MongoDB
+const url = 'mongodb://localhost/prison_nantes';
 
 const app = express();
 app.use(express.static('pages'));
@@ -21,6 +24,11 @@ app.post('/prisoniers', (req, res) => {
 
 app.post('/affaires', (req, res) => {
     res.sendFile(path.join(__dirname + '/pages/affaires.html'));
+});
+
+mongoClient.connect(url, (err, db) => {
+  console.log('Connected to DB prison_nantes');
+  db.close();
 });
 
 app.listen(port);
